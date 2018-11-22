814384bb9e817b2ed90c6ff9a7356eaa53e98c87
kimseowoo <swoo422@naver.com>
2018-11-22 16:15:45 +0900

    5

--- main.c
+++ main.c
@@ -5,17 +5,33 @@
 
 int main(int argc, char *argv[]) {
 	
-	FILE *fp;
-	char input[100];
+	FILE *fp,*fp2;
+	char input;
+	char original[100];
+	char copy[100];
+	int i=0;
 	
-	fp = fopen("sample.txt","r");
-	 
-	while (fgets(input, 100, fp) != NULL)
+	
+	printf("original file : ");
+	scanf("%s",original);
+	printf("copy file : ");
+	scanf("%s",copy);
+	
+	
+	fp = fopen(original,"r");
+	fp2 = fopen(copy, "w");
+	
+	
+	while ((input=fgetc(fp))!= EOF)
 	{
-		printf(input);
+		fputc(input, fp2);
+		i++;
 	}
 	
-	fclose(fp);
+	printf("Copy succeed!(%i Bytes copied)\n",i);
 	
+	fclose(fp);
+	fclose(fp2);
+
 	return 0;
 }
